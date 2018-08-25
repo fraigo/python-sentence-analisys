ddedc0d6da3ac2b17a7414801d3c1e734da523b0
Francisco Igor <franciscoigor@gmail.com>
2018-08-24 20:08:51 -0700

    Added sentence count and sorting

--- sentences.py
+++ sentences.py
@@ -1,10 +1,11 @@
 import unidecode
+import pprint
 
-#import nltk
+import nltk
 #from nltk.corpus import cess_esp as cess
 
 #nltk.download('punkt')
-#nltk.download('averaged_perceptron_tagger')
+#nltk.download('averaged_perceptron_taagger')
 #nltk.download('maxent_ne_chunker')
 #nltk.download('words')
 
@@ -18,27 +19,83 @@ def normalizeChars(line):
     line = line.lower()
     return line
 
-def removeStopWords(line):
-    stopwords = ['el', 'la', 'los', 'las', 'le',
-                 'un', 'una', 'unos', 'unas',
-                 'por', 'que', 'con', 'de', 'del', 'para', 'a',
-                 'si',
-                 'es']
+def removeStopWords(stopWords, line):
     querywords = line.split()
-    resultwords = [word for word in querywords if word.lower() not in stopwords]
+    resultwords = [word for word in querywords if word.lower() not in stopWords]
     return ' '.join(resultwords)
 
-# open file
+def sortedChunks(list, n):
+    for i in range(0, len(list) - n, 1):
+        yield ' '.join(sorted(list[i:i + n]))
+
+def chunks(list, n):
+    for i in range(0, len(list) - n, 1):
+        yield ' '.join(list[i:i + n])
+
+def sortingCount(value):
+    return value
+
+def dropValues(items, max):
+    result = dict(items)
+    for key, value in items.items():
+        if value <= max:
+            del result[key]
+    return result
+
+def sortByValue(items, reverse):
+    return sorted(items.items(), key=lambda x: x[1], reverse=reverse)
+
+def appendStats(stats, detail, words, line, count):
+    sortedPhrases = list(sortedChunks(words, count))
+    phrases = list(chunks(words, count))
+    for phrase in sortedPhrases:
+        if phrase in stats:
+            stats[phrase] += 1
+            detail[phrase].append(line)
+        else:
+            stats[phrase] = 1
+            detail[phrase] = []
+
+    return stats
+
+# open file and read lines
 file = open("sentences.txt")
+# lines = file.readlines()
+lines = file.read().splitlines()
 
-# read lines
-lines = file.readlines()
+file = open("stopwords-en.txt")
+stopWords = file.read().splitlines()
 
-for x in lines:
-    x = removeNonWords(x)
-    x = normalizeChars(x)
-    x = removeStopWords(x)
-    print (x)
+phrases = {}
+details= {}
+
+for line in lines:
+    #tokens = nltk.word_tokenize(x)
+    #tags = nltk.pos_tag(tokens)
+    tmp = removeNonWords(line)
+    tmp = normalizeChars(tmp)
+    tmp = removeStopWords(stopWords,tmp)
+    words = tmp.split()
+    phrases = appendStats(phrases, details, words, line,  3)
+    phrases = appendStats(phrases, details, words, line, 4)
+
+phrases = dropValues(phrases, 2)
+
+sortedPhrases = sortByValue(phrases, reverse=True)
+
+#pprint.pprint(sortedPhrases)
+
+for key, value in sortedPhrases:
+    print("\n[" + key + "] " + str(value) + " ocurrences:" )
+    for line in details[key]:
+        print(line)
+
+
+
+
+
+
+
 
 
 
